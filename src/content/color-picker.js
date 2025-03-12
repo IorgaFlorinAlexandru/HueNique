cbed9ae5506fa789bfe93be22262867b0fff0725
IorgaFlorinAlexandru <iorgaflorinalexandru@gmail.com>
2025-03-12 21:56:48 +0200

    feat: Hex code from color picked

--- src/content/color-picker.js
+++ src/content/color-picker.js
@@ -2,12 +2,13 @@ const eyeDropper = new HueniqueEyeDropper();
 
 function huenique() {
   eyeDropper.open().then((color) => {
-    navigator.clipboard.writeText(color);
+    console.log(color);
+    navigator.clipboard.writeText("#"+color.hex);
 
     const square = document.createElement("div");
     square.style.width = "100px";
     square.style.height = "100px";
-    square.style.background = color;
+    square.style.background = "#"+color.hex;
     document.body.appendChild(square);
   });
 }
--- src/utils/colors.util.js
+++ src/utils/colors.util.js
@@ -1,12 +1,17 @@
 class Colors {
-  constructor() {}
+  constructor() { }
 
   static extractPixelColor(canvas, x, y) {
     const ctx = canvas.getContext("2d");
     const pixel = ctx.getImageData(x, y, 1, 1);
     const data = pixel.data;
-    console.log(data);
+    
+    const uint8Array = new Uint8Array(data.slice(0,3));
 
-    return `rgb(${data[0]}, ${data[1]}, ${data[2]})`;
+    const rgbColor = `rgb(${data[0]}, ${data[1]}, ${data[2]})`;
+    return { 
+      rgb: rgbColor,
+      hex: uint8Array.toHex()
+    };
   }
 }
