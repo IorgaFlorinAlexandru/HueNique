e535c923e1799b4cec9ffa4a484311d81ef464c3
IorgaFlorinAlexandru <iorgaflorinalexandru@gmail.com>
2025-04-01 19:40:51 +0300

    Functional popup tabs are now changing the content

---
+++ src/popup/popup.js
@@ -0,0 +1,24 @@
+const TAB_ACTIVE_CLASS = "tab-button__active";
+const tabButtons = document.body.querySelectorAll(".tab-button");
+
+for (let i = 0; i < tabButtons.length; i++) {
+  const btn = tabButtons[i];
+
+  btn.addEventListener("click", () => {
+    changeTab(btn);
+  });
+}
+
+function changeTab(button) {
+  if (button.classList.contains(TAB_ACTIVE_CLASS)) return;
+
+  const multiplier = [...tabButtons].findIndex((b) => b === button);
+  const content = document.body.querySelector(".huenique-popup__content");
+  content.style.translate = `${multiplier * -335}px 0px`;
+
+  tabButtons.forEach((b) => {
+    b.classList.remove(TAB_ACTIVE_CLASS);
+  });
+
+  button.classList.add(TAB_ACTIVE_CLASS);
+}
