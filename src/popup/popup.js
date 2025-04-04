7e8b31b39893e25eaf6e1636ce47e8c3a7842192
IorgaFlorinAlexandru <iorgaflorinalexandru@gmail.com>
2025-04-05 00:44:46 +0300

    feat: Recent picked color is shown in popup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -10,9 +10,8 @@ tabs.forEach((tab) => {
 
 browser.storage.local.get("huenique").then((storage) => {
   const recent = storage.huenique.colors.recent;
-  if(recent !== null) {
-    const container = document.body.querySelector("#recentColor");
-    console.log(recent);
+  if (recent !== null) {
+    showRecentColorSection(recent);
   }
 });
 
@@ -28,4 +27,49 @@ function changeTab(tab) {
   });
 
   tab.classList.add(TAB_ACTIVE_CLASS);
+}
+
+function showRecentColorSection(color) {
+  const container = document.body.querySelector(".color__recent");
+  container.style.display = "block";
+
+  const swatch = container.querySelector(".color__details__swatch");
+  swatch.style.backgroundColor = color.hex.string;
+
+  const code = container.querySelector(".color__details__content__code");
+  code.innerText = color.hex.string;
+
+  container.querySelector(".copy").onclick = () => {
+    navigator.clipboard.writeText(color.hex.string);
+  }
+
+  container.querySelector(".save").onclick = () => {
+    console.error("Not implemented yet");
+  }
+
+  container.querySelector(".favorite").onclick = () => {
+    console.error("Not implemented yet");
+  }
+
+  const formats = container.querySelector(".color__formats");
+  [color.rgb, color.hsl].forEach((f) => {
+    const div = document.createElement("div");
+    div.classList.add("color__format");
+
+    const h4 = document.createElement("h4");
+    h4.classList.add("color__format__code");
+    h4.innerText = f.string;
+
+    const button = document.createElement("button");
+    button.classList.add("button", "color__format__button");
+    const svg = container.querySelector(".copy-svg");
+    button.appendChild(svg.cloneNode(true));
+    button.onclick = () => {
+      navigator.clipboard.writeText(f.string);
+    };
+
+    div.appendChild(h4);
+    div.appendChild(button);
+    formats.appendChild(div);
+  });
 }
